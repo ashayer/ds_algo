8db2f566f18328a4faeb08fbb609df0d9c3684f1
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-31 20:18:21 -0700

    Create AlgoCard.tsx

---
+++ src/components/HomeCards/AlgoCard.tsx
@@ -0,0 +1,35 @@
+import { Typography, Grid, Button, Divider } from "@mui/material";
+import styles from "./HomeCardsStyles";
+import GridCardItem from "./GameCardItem";
+const StatsCard = () => {
+
+  return (
+    <>
+      <Typography variant="h3" fontWeight="bold">
+        SORTING ALGORITHMS
+      </Typography>
+      <Divider />
+      <Grid container sx={{ ...styles.homeCardContainer }}>
+        <GridCardItem>
+          {/* <Typography>{`${calculateCompletedReadingForAlgo()}% Read`}</Typography> */}
+        </GridCardItem>
+        <GridCardItem width="45%" alignSelf="end">
+          <Button
+            fullWidth
+            variant="outlined"
+            color="info"
+          >
+            <Typography variant="h3">Read</Typography>
+          </Button>
+        </GridCardItem>
+        <GridCardItem width="45%" alignSelf="end">
+          <Button fullWidth variant="contained">
+            <Typography variant="h3">Sandbox</Typography>
+          </Button>
+        </GridCardItem>
+      </Grid>
+    </>
+  );
+};
+
+export default StatsCard;
