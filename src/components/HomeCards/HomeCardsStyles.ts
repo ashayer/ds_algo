f6d0f5591a85492162425422e758af00953c7184
Aran Hayer <ashayer@users.noreply.github.com>
2022-08-31 20:18:27 -0700

    Create HomeCardsStyles.ts

---
+++ src/components/HomeCards/HomeCardsStyles.ts
@@ -0,0 +1,10 @@
+const styles = {
+  homeCardContainer: {
+    alignItems: "center",
+    direction: "column",
+    minHeight: "30vh",
+    justifyContent: "space-around",
+  },
+};
+
+export default styles;
