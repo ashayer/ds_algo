0f57bac5061e88a879d9d9700c4e120d0c813ef1
Aran Hayer <ashayer@users.noreply.github.com>
2022-09-22 22:50:22 -0700

    template bottom landing page section

--- src/components/LandingPage/BottomLandingPage.tsx
+++ src/components/LandingPage/BottomLandingPage.tsx
@@ -8,32 +8,47 @@ const BottomLandingPage = () => {
       direction="column"
       sx={{ backgroundColor: "#121212", color: "white", p: 10 }}
     >
-      <Grid item>
-        <Typography variant="h2" fontWeight="900">
-          READINGS
-        </Typography>
-        <Typography variant="h6" fontWeight="bold">
-          Learn about fundamental sorting algorithms and data structures with with sequential
-          reading sections and summary quizzes
-        </Typography>
+      <Grid item container sx={{ textAlign: "right" }}>
+        <Grid item sx={{ border: "1px solid red" }} xs={6}>
+          picture or gif
+        </Grid>
+        <Grid item sx={{ border: "1px solid red" }} xs={6}>
+          <Typography variant="h2" fontWeight="900">
+            READINGS
+          </Typography>
+          <Typography variant="h6" fontWeight="bold">
+            Learn about fundamental sorting algorithms and data structures with with sequential
+            reading sections and summary quizzes
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h2" fontWeight="900">
-          QUIZ GAME
-        </Typography>
-        <Typography variant="h6" fontWeight="bold">
-          Keep your skills sharp with a quiz game with 50+ randomly generated questions covering all
-          reading topics.
-        </Typography>
+      <Grid item container>
+        <Grid item sx={{ border: "1px solid red" }} xs={6}>
+          <Typography variant="h2" fontWeight="900">
+            READINGS
+          </Typography>
+          <Typography variant="h6" fontWeight="bold">
+            Learn about fundamental sorting algorithms and data structures with with sequential
+            reading sections and summary quizzes
+          </Typography>
+        </Grid>
+        <Grid item sx={{ border: "1px solid red" }} xs={6}>
+          picture or gif
+        </Grid>
       </Grid>
-      <Grid item>
-        <Typography variant="h2" fontWeight="900">
-          SANDBOX
-        </Typography>
-        <Typography variant="h6" fontWeight="bold">
-          EXPLORE SORTING ALGORITHMS THROUGH VISUALIZATIONS AND STEP-THROUGH CODE HIGHLIGHTING FOR A
-          DEEPER UNDERSTANDING
-        </Typography>
+      <Grid item container sx={{ textAlign: "right" }}>
+        <Grid item sx={{ border: "1px solid red" }} xs={6}>
+          picture or gif
+        </Grid>
+        <Grid item sx={{ border: "1px solid red" }} xs={6}>
+          <Typography variant="h2" fontWeight="900">
+            READINGS
+          </Typography>
+          <Typography variant="h6" fontWeight="bold">
+            Learn about fundamental sorting algorithms and data structures with with sequential
+            reading sections and summary quizzes
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
