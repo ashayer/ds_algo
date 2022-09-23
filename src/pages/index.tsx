22143dc9fdda95819bfad9a877bd74dafba11c95
Aran Hayer <ashayer@users.noreply.github.com>
2022-09-22 22:17:32 -0700

    draft of first section of landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { NextPage } from "next";
 // import { useNavigate } from "react-router-dom";
 // import Signup from "../components/Signup/Signup";
 // import useStore from "../../stores/authStore";
-
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
@@ -23,9 +23,62 @@ const LandingPage: NextPage = () => {
 
   return (
     <Grid container direction="column" sx={{ overflowX: "hidden" }}>
-      <Grid item sx={{ backgroundColor: "#FE6812", height: "95vh", width: "100vw", pt: 10 }}>
-        <Typography variant="h1">DSALGO</Typography>
-        <Image src={educate} alt="Educator image" />
+      <Grid
+        container
+        item
+        sx={{
+          backgroundColor: "#FE6812",
+          height: "95vh",
+          border: "2px solid blue",
+          textAlign: "center",
+        }}
+      >
+        <Grid
+          item
+          container
+          direction="column"
+          sx={{ border: "2px solid red", pt: 10, alignItems: "center" }}
+          spacing={3}
+          xs={6}
+        >
+          <Grid item>
+            <Typography
+              variant="h1"
+              sx={{
+                fontWeight: "bold",
+                textDecoration: "underline",
+                color: "white",
+                textUnderlineOffset: "1rem",
+                textDecorationThickness: "5px",
+              }}
+            >
+              DSALGO
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="h4"
+              sx={{
+                color: "white",
+                fontWeight: 900,
+              }}
+            >
+              Supplement your learning
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              sx={{ backgroundColor: "#121212", borderRadius: "10px", p: 2, mt: 10 }}
+              endIcon={<ArrowForwardIcon />}
+            >
+              <Typography variant="h6">Start Learning</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} sx={{ border: "2px solid red", pt: 10 }}>
+          <Image src={educate} alt="Educator image" />
+        </Grid>
       </Grid>
       <Grid item sx={{ backgroundColor: "#272838", height: "40vh", width: "100vw" }}></Grid>{" "}
       <Grid item sx={{ backgroundColor: "#121212", height: "100vh", width: "100vw" }}></Grid>
